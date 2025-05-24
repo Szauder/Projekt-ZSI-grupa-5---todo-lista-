682199f5993658b8dd0196ffed195367415b74a8
STANYSLA <s2cieslak@outlook.com>
2025-05-24 21:44:50 +0200

    Help command

--- ConsoleTodoApp/ConsoleTodoApp/CommandsRegister.cpp
+++ ConsoleTodoApp/ConsoleTodoApp/CommandsRegister.cpp
@@ -9,4 +9,14 @@ CommandBase* CommandsRegister::GetCommandByExecName(const std::string& name) {
     return it != CommandsRegister::commands.end() ? it->second : nullptr;
 }
 
+std::vector<std::string> CommandsRegister::GetAvaibleCommandsNames()
+{
+    auto names = std::vector<std::string>();
+
+    for (auto pair : commands)
+        names.push_back(pair.first);
+
+    return names;
+}
+
 std::unordered_map<std::string, CommandBase*> CommandsRegister::commands = std::unordered_map<std::string, CommandBase*>();
